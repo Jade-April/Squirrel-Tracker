3985c9337d36a894da6007b5a76f2ef86c952622
Junyao Wang <jw3845@columbia.edu>
2019-12-08 04:33:46 +0000

    Add map views

--- squirrel/views.py
+++ squirrel/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Squirrel
-
+import random
 
 def list_sightings(request):
     squirrel=Squirrel.objects.all()
@@ -14,4 +14,18 @@ def list_sightings(request):
             }
     return render(request,'squirrel/list.html',context)
 
+def get_map(request):
+    squirrel=Squirrel.objects.all()
+    myitems=[]
+    for i in range(50):
+        j=random.randint(0,len(squirrel))
+        item=list(squirrel)[j]
+        item_list=[float(item.Longitude),float(item.Latitude)]
+        myitems.append(item_list)
+    title='squirrel map'
+    context={
+            'title':title,
+            'myItems':myitems,
+            }
+    return render(request,'squirrel/address.html',context)
 # Create your views here.
