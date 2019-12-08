ec4725b278e710ff55abb84c47f0a4c78dfaf747
Hanyu <hl3304@columbia.edu>
2019-12-08 04:07:18 +0000

    add sightings view

--- squirrel/views.py
+++ squirrel/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Squirrel
+
+
+def list_sightings(request):
+    squirrel=Squirrel.objects.all()
+    link_list=[]
+    for s in squirrel:
+        link_list.append(s.Unique_Squirrel_ID)
+    title='list of sightings'
+    context={
+            'title':title,
+            'link_list':link_list,
+            }
+    return render(request,'squirrel/list.html',context)
 
 # Create your views here.
