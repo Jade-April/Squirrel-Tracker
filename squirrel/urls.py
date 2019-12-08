458f20364f5470ced19a0be55c13a6f98de4a1d6
EvelynLi1116 <56941157+EvelynLi1116@users.noreply.github.com>
2019-12-08 11:32:57 -0500

    update urls

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('sightings/',views.list_sightings),
     path('map/',views.get_map),
     path('sightings/add',views.add),
+    path('sightings/<unique_squirrel_id>',views.update),
 ]
